91142919c747dc600025015d39851940331414a8
Amaari <arish2k3@gmail.com>
2023-04-17 11:26:24 +0530

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 import userEvent from "@testing-library/user-event";
 import "./index.css";
 
+function AdminPanel() {
+  return (
+    <>
+      <p>This is an admin panel</p>
+    </>
+  );
+}
+
+function LoginForm() {
+  return (
+    <>
+      <p>This is a login form</p>
+    </>
+  );
+}
+
+let content;
+let isLoggedIn = true;
+
+if (isLoggedIn) {
+  content = <AdminPanel />;
+} else {
+  content = <LoginForm />;
+}
+
 function MyButton() {
   return <button>I'm a button</button>;
 }
@@ -31,6 +56,7 @@ function App() {
         <h1>Welcome to my React App</h1>
         <MyButton />
       </div>
+      <div>{content}</div>
       <div>
         <AboutPage />
       </div>
