6ca930d6fc166a8556a84b9df0285f7a07d15020
Amaari <arish2k3@gmail.com>
2023-04-17 11:52:39 +0530

    handle clicks

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,10 @@ let content;
 let isLoggedIn = false;
 
 function MyButton() {
-  return <button>I'm a button</button>;
+  function handleClick() {
+    alert("You clicked me!");
+  }
+  return <button onClick={handleClick}>I'm a button</button>;
 }
 
 function AboutPage() {
